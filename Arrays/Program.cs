4e345ea6faf8b77e7e83be040d4a347c54659e54
Viktoria Rosenfeld <viktoria.rosenfeld@tktk.ee>
2025-12-15 19:50:26 +0200

    Добавьте файлы проекта.

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket");
+
+for ( int i = 0; i < fruitBasket.Length; i++ )
+{
+    Console.WriteLine( fruitBasket[i]);
+}
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[2] = "peaches";
+fruitBasket[3] = "melon";
+
+
+
+for ( int i = 0;i < fruitBasket.Length;i++ )
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringandArrays/Program.cs
@@ -0,0 +1,11 @@
+﻿string userName = "Darth Vader";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+
+//replace
+//a - 4
+
+string updatedUserName = userName.Replace('a', '4');
+
+Console.WriteLine(updatedUserName);
